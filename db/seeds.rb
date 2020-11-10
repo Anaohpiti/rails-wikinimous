ce92b353bada82aaa4326463302a767d9692b1d9
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-10 16:48:30 +0100

    wikimedia fonctioning

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title:    Faker::Games::Pokemon.name,
+    content: "#{Faker::Games::Pokemon.location}, #{Faker::Games::Pokemon.move}",
+      )
+  article.save!
+end
+puts 'Finished!'
